f6a2f638754f9490678e062e9edff9d854069667
aleksejpodkidysev <alexp2019@gmail.com>
2020-08-21 23:25:41 +0300

    Updates

---
+++ precache-manifest.5270c8752b69561afb6eb3ce846d2ed9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5f5ecb238811042984f8f1ea69d032",
+    "url": "/AlekseyPodkid/index.html"
+  },
+  {
+    "revision": "0bec09abb71d5277b48f",
+    "url": "/AlekseyPodkid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0efb8114bb9f3c34e3f",
+    "url": "/AlekseyPodkid/static/js/2.31bc40bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlekseyPodkid/static/js/2.31bc40bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bec09abb71d5277b48f",
+    "url": "/AlekseyPodkid/static/js/main.b45d5341.chunk.js"
+  },
+  {
+    "revision": "d0d59682c7563735fc74",
+    "url": "/AlekseyPodkid/static/js/runtime-main.277cf9d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AlekseyPodkid/static/media/logo.5d5d9eef.svg"
+  }
+]);
